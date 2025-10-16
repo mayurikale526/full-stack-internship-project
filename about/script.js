1e7db03e9de2c5cda2604d16efd3e719f817ae7e
mayurikale526 <mayurikale526@gmail.com>
2025-10-16 18:23:41 +0530

    Add files via upload

---
+++ about/script.js
@@ -0,0 +1,19 @@
+gsap.to(".image", {
+    backgroundPosition: "50%  0%", 
+    scrollTrigger: {
+      trigger: ".image",
+      start: "top bottom",   
+      end: "bottom top",     
+      scrub: true,           
+    }
+});
+
+gsap.to(".get-started", {
+    backgroundPosition: "50%  0%", 
+    scrollTrigger: {
+      trigger: ".get-started",
+      start: "top bottom",   
+      end: "bottom top",     
+      scrub: true,           
+    }
+});
